b7b984b9aa0f6837ab3533503bd4c923d4d01d7a
awanla <48355798+awanla@users.noreply.github.com>
2019-03-30 13:38:17 +0800

    測試少了ICONTOP

--- js/all.js
+++ js/all.js
@@ -2,38 +2,38 @@ $(document).ready(function () {
 
     // header color change
     $(window).scroll(function () {
-        var scrollDistance = $(window).scrollTop();
+        // var scrollDistance = $(window).scrollTop();
 
-        if (scrollDistance > 90) {
-            $(".gototop").fadeIn(1000);
-        }
-        else {
-            $(".gototop").fadeOut();
-        }
+        // if (scrollDistance > 90) {
+        //     $(".gototop").fadeIn(1000);
+        // }
+        // else {
+        //     $(".gototop").fadeOut();
+        // }
         var scrollDistance = $(window).scrollTop();
-        if (scrollDistance > 350) {
-            $(".news_text").css("animation-play-state", "running");
-            $(".news_box").css("animation-play-state", "running");
-        }
+            if (scrollDistance > 350) {
+                $(".news_text").css("animation-play-state", "running");
+                $(".news_box").css("animation-play-state", "running");
+            }
         var scrollDistance = $(window).scrollTop();
-        if (scrollDistance > 1250) {
-            $(".about_skew_animation").css("animation-play-state", "running");
-            $(".about_title").css("animation-play-state", "running");
-        }
+            if (scrollDistance > 1250) {
+                $(".about_skew_animation").css("animation-play-state", "running");
+                $(".about_title").css("animation-play-state", "running");
+            }
         var scrollDistance = $(window).scrollTop();
-        if (scrollDistance > 2100) {
-            $(".skew_animate").css("animation-play-state", "running");
-            $(".service_pic .pic").css("animation-play-state", "running");
-            $(".service_tilte").css("animation-play-state", "running");
-            
-        }
+            if (scrollDistance > 2100) {
+                $(".skew_animate").css("animation-play-state", "running");
+                $(".service_pic .pic").css("animation-play-state", "running");
+                $(".service_tilte").css("animation-play-state", "running");
+                
+            }
         var scrollDistance = $(window).scrollTop();
-        if (scrollDistance > 3000) {
-            $(".contect_animate").css("animation-play-state", "running");
-            $(".container_title h1").css("animation-play-state", "running");
-            $(".container_text").css("animation-play-state", "running");
+            if (scrollDistance > 3000) {
+                $(".contect_animate").css("animation-play-state", "running");
+                $(".container_title h1").css("animation-play-state", "running");
+                $(".container_text").css("animation-play-state", "running");
 
-        }
+            }
     });
     
     $('.gototop').click(function () {
